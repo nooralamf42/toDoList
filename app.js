3957af73204999ab75912f5db91ad406ae001e83
Noor Alam <90553481+nooralamf42@users.noreply.github.com>
2023-04-06 01:18:14 +0530

    Update app.js
    added audio

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const taskBtn = document.getElementById("taskBtn")
 const previousData = document.querySelector(".data")
 const tasksBox = document.getElementById("tasksBox")
 
+const addAudio = new Audio("https://cdn.pixabay.com/download/audio/2022/10/30/audio_6634b0add4.mp3?filename=click-124467.mp3")
+const taskCompletedAudio = new Audio("https://cdn.pixabay.com/download/audio/2023/02/28/audio_52ccaf1a85.mp3?filename=click-button-140881.mp3")
 
 taskBtn.onclick = () =>{
+    addAudio.pause()
+    addAudio.currentTime = 0
+    addAudio.play()
     const taskName = document.getElementById("taskName").value
     const taskDescription = document.getElementById("taskDescription").value
     const taskDate = document.getElementById("taskDate").value
@@ -30,6 +35,9 @@ taskBtn.onclick = () =>{
     const taskContent = document.querySelectorAll(".taskContent")
     taskCompleted.forEach((value,index) => {
         value.onclick = () => {
+            taskCompletedAudio.pause()
+            taskCompletedAudio.currentTime = 0
+            taskCompletedAudio.play()
             taskCompleted[index].innerHTML = "✓"
             taskCompleted[index].style.backgroundColor = "#0174fe"
             newTaskName[index].style.textDecoration = "line-through"
